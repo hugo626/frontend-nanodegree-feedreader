3fa05383934e8a8cba3de474e3de93a6b50dfa24
Yuguo Li <hugo626@gmail.com>
2017-06-07 00:42:34 +0800

    add test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -10,9 +10,9 @@
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -27,46 +27,97 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('every feed entry should have url defined, and it is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).toBeTruthy();
+            });
+        });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('every feed entry should have name defined, and it is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).toBeTruthy();
+            });
+        });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* A new test suite named "The menu" */
+    describe('The menu', function() {
+        var menuHiddenTag = 'menu-hidden';
+        var feedListClass = '.feed-list';
+        var menuButtonClass = '.menu-icon-link';
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        beforeEach(function() {
+            resetMenu();
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        afterEach(function() {
+            resetMenu();
+        });
 
+        var resetMenu = function() {
+            if (!$('body').hasClass(menuHiddenTag)) {
+                $('body').addClass(menuHiddenTag);
+            }
+        }
+
+        // /* TODO: Write a test that ensures the menu element is
+        //  * hidden by default. You'll have to analyze the HTML and
+        //  * the CSS to determine how we're performing the
+        //  * hiding/showing of the menu element.
+        //  */
+        it('should be hidden by default', function() {
+            expect($('body').hasClass(menuHiddenTag)).toBeTruthy();
+        });
+
+        // /* TODO: Write a test that ensures the menu changes
+        //  * visibility when the menu icon is clicked. This test
+        //  * should have two expectations: does the menu display when
+        //  * clicked and does it hide when clicked again.
+        //  */
+        it('should be displayed when menu button is clicked, if the menu was hidden.', function() {
+            // first of all make sure the menu is hidden.
+            expect($('body').hasClass(menuHiddenTag)).toBeTruthy();
+            $(menuButtonClass).click();
+            expect($('body').hasClass(menuHiddenTag)).toBeFalsy();
+        });
+
+        it('should be hidden when menu button is clicked, if the menu was displayed.', function() {
+            // first of all make sure the menu is hidden.
+            $(menuButtonClass).click().click();
+            expect($('body').hasClass(menuHiddenTag)).toBeTruthy();
+        });
+
+        it('should be hidden when menu list is clicked, if the menu was displayed.', function() {
+            // first of all make sure the menu is hidden.
+            $(menuButtonClass).click();
+            $(feedListClass).click();
+            expect($('body').hasClass(menuHiddenTag)).toBeTruthy();
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
 }());
